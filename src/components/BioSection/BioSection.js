6d0f407533c8154fe7166bfa6596a6d4de8d28e5
Ryan Walter <rwalter215@gmail.com>
2018-01-11 18:53:43 -0600

    image rounded on about us page

--- src/components/BioSection/BioSection.js
+++ src/components/BioSection/BioSection.js
@@ -6,7 +6,7 @@ import './BioSection.css';
 const BioSection = ({ name, text, url }) => (
   <div className="BioSection">
     <h2>{name}</h2>
-    <Image src={url} size="medium" shape="rounded" floated="left" />
+    <Image src={url} size="medium" rounded floated="left" />
     <p>{text}</p>
   </div>
 );
